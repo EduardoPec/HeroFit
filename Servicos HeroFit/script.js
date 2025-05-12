53f1c04285c962bc9a84211e3d2f101406353bc3
brunonovais05 <brunoncsimoes@gmail.com>
2025-05-11 22:31:54 -0300

    feat: adicionar menu hamburguer responsivo na tela Serviços

--- Servicos HeroFit/script.js
+++ Servicos HeroFit/script.js
@@ -1,26 +1,29 @@
-
-document.querySelectorAll('.btn').forEach((button, index) => {
-    
+document.addEventListener("DOMContentLoaded", () => {
+  // Botão Ler Mais
+  document.querySelectorAll('.btn').forEach((button, index) => {
     button.addEventListener('click', (event) => {
-        event.preventDefault(); 
+      event.preventDefault();
 
-       
-        let message;
-        switch (index) {
-            case 0:
-                message = "Mais informações sobre Musculação estarão disponíveis em breve!";
-                break;
-            case 1:
-                message = "Mais informações sobre os treinos para Homens estarão disponíveis em breve!";
-                break;
-            case 2:
-                message = "Mais informações sobre os treinos para Mulheres estarão disponíveis em breve!";
-                break;
-            default:
-                message = "Mais informações estarão disponíveis em breve!";
-        }
-
-        
-        alert(message);
+      let message;
+      switch (index) {
+        case 0:
+          message = "Mais informações sobre Musculação estarão disponíveis em breve!";
+          break;
+        case 1:
+          message = "Mais informações sobre os treinos para Homens estarão disponíveis em breve!";
+          break;
+        case 2:
+          message = "Mais informações sobre os treinos para Mulheres estarão disponíveis em breve!";
+          break;
+        default:
+          message = "Mais informações estarão disponíveis em breve!";
+      }
+      alert(message);
     });
+  });
+
+  // Botão Hamburguer
+  document.querySelector(".hamburguer").addEventListener("click", () => {
+    document.querySelector(".menu-header").classList.toggle("ativo");
+  });
 });
